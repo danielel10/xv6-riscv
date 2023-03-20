f2865b57c6edef2bd46a1eb1313be68fe144f344
Daniel Elhadad <delhadad@dalet.com>
2023-03-20 20:27:07 +0000

    dont task 2 (need to check free with debug), task 0 is task 1

---
+++ user/memsize_test.c
@@ -0,0 +1,16 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  fprintf(1, "Curr proc size is:%x\n", memsize());
+  void* pt = malloc(20000);
+  fprintf(1, "Curr proc size is:%x\n", memsize());
+  
+  free(pt);
+  fprintf(1, "Curr proc size is:%x\n", memsize()); 
+  
+  exit(0);
+}
--- user/user.h
+++ user/user.h
@@ -22,6 +22,7 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+int memsiz(void);
 
 // ulib.c
 int stat(const char*, struct stat*);
