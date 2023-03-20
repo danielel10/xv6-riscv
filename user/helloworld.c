1645d0505fcb971086f0fb531c4ae2a6d2276afd
Daniel Elhadad <delhadad@dalet.com>
2023-03-20 18:38:14 +0000

    task 0 done

---
+++ user/helloworld.c
@@ -0,0 +1,12 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  write(1, "Hello World xv6\n", strlen("Hello World xv6\n"));
+    
+  
+  exit(0);
+}
